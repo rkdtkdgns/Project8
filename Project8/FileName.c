b084b5ef9e6e7ddc349ff741e679fa9711819af1
user <user@DESKTOP-RH0EED9>
2023-08-04 22:02:36 +0900

    2023-08-04

---
+++ Project8/FileName.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int 스트라이크, 아웃, 베이스 = 0;
+	int a, b,d;
+		printf("숫자야구");
+		printf("\n");
+			for (a=1; a<=9; a++)
+			{
+				printf("라운드:%d\n", a);
+				scanf("%d", &d);
+				return 0;
+			}
+}
