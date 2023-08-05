60bfbb28e6cab011f895577b276f00b2a3af36ac
user <user@DESKTOP-RH0EED9>
2023-08-06 00:19:15 +0900

    2023-08-06

--- Project8/FileName.c
+++ Project8/FileName.c
@@ -2,13 +2,25 @@
 int main()
 {
 	int 스트라이크, 아웃, 베이스 = 0;
-	int a, b,d;
+	int a, b, d, i = 0;
 		printf("숫자야구");
 		printf("\n");
-			for (a=1; a<=9; a++)
+		srand(time(NULL));
+		for (int i = 0; i < 3; i++)
+		{
+			int random = rand() % 5;
+			printf("%d ", random);
+		}
+			for (a=1; a<8; a++)
 			{
-				printf("라운드:%d\n", a);
-				scanf("%d", &d);
-				return 0;
+				do
+				{
+					printf("라운드:%d\n", a++);
+					scanf("%d", &d);
+					printf("\n");
+				}while (d > 0.001);
+				printf("다시 작성하세요");
+				printf("\n");
+			
 			}
 }
